c0ddb6db5217d50bd8662807ccdef18ca2647d37
Akhil Vyas <akhilvimalvyas@gmail.com>
2019-11-13 00:16:20 -0600

    Basic search functionality with linear layout with image and 2 textedit

---
+++ app/src/main/java/com/example/concert_android/Videos.java
@@ -0,0 +1,13 @@
+package com.example.concert_android;
+
+public class Videos {
+    private String VideoName;
+
+    public Videos(String VideoName) {
+        this.VideoName = VideoName;
+    }
+
+    public String getAnimalName() {
+        return this.VideoName;
+    }
+}
